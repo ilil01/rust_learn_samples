ce54b0723116446110ef3af35b0a74dd76294640
Shliapin Ilia <shliapin.ilia@yandex.ru>
2021-11-04 01:20:50 +0300

    finished enums
    Signed-off-by: Shliapin Ilia <shliapin.ilia@yandex.ru>

--- enums.rs
+++ enums.rs
@@ -1,12 +1,15 @@
 enum IpAddr {
     V4(u8, u8, u8, u8),     // So not to mess with struct
     V6(String),
+    NonValid,
 }
 
 impl IpAddr {
     fn print(&self) {
         match self {
-            IpAddr::V4 => println!("{}:{}:{}:{}", self.);
+            IpAddr::V4(a1, a2, a3, a4) => println!("{}:{}:{}:{}", a1, a2, a3, a4),
+            IpAddr::V6(s)              => println!("{}", s),
+            _                          => println!("Invalid IP!")   /* marked unused, other names could be used */
         }
     }
 }
@@ -15,4 +18,16 @@ fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
 
     let loopback = IpAddr::V6(String::from("::1"));
+    home.print();
+    loopback.print();
+
+    let inval = IpAddr::NonValid;
+    inval.print();
+
+    if let IpAddr::V4(a1, a2, a3, a4) = inval {
+        println!{"{}", a1 + a2 + a3 + a4};
+    }
+    if let IpAddr::V4(a1, a2, a3, a4) = home {
+        println!{"{}", a1 + a2 + a3 + a4};
+    }
 }
